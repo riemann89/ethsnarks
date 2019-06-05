818979d931e581bd22544bded28e6f4369b58a2b
riemann89 <gg@clearmatics.com>
2019-06-05 19:30:56 +0100

    sha3 constants generation

--- src/utils/mimc_hash_test_cases.cpp
+++ src/utils/mimc_hash_test_cases.cpp
@@ -71,8 +71,10 @@ void generate_mt(int depth){
         std::cout << "++++ Level: " << depth - i << " to " << depth - i -1 << "+++" << std::endl;
         std::cout << "m0 =";
         m_0_f.as_bigint().print();
+        std::cout << std::endl;
         std::cout << "m1 =";
         m_1_f.as_bigint().print();
+        std::cout << std::endl;
         std::cout << "out = ";
         result.as_bigint().print();
         std::cout << std::endl;
@@ -80,6 +82,50 @@ void generate_mt(int depth){
         m_0_f = result;    
         m_1_f = result;
     }
+}
+
+// This function generates mimchash merklee tree of a given depth where leafs nodes are equal to 0. It use the mimchash gadget with iv = 0.
+void generate_sha3_constants(){
+        const size_t DIGEST_SIZE_BYTES = 32;
+        unsigned char output_digest[DIGEST_SIZE_BYTES];
+
+        // Hash the initial seed
+        sha3_context ctx;
+        sha3_Init256(&ctx);
+        sha3_Update(&ctx, MIMC_SEED, strlen(MIMC_SEED));
+        memcpy(output_digest, sha3_Finalize(&ctx), DIGEST_SIZE_BYTES);
+
+        std::cout << "std::vector<FieldT>& round_constants = [";
+
+        for( int i = 0; i < MIMC_ROUNDS; i++ )
+        {
+            // Derive a sequence of hashes to use as round constants
+            sha3_Init256(&ctx);
+            sha3_Update(&ctx, output_digest, DIGEST_SIZE_BYTES);
+            memcpy(output_digest, sha3_Finalize(&ctx), DIGEST_SIZE_BYTES);
+
+            // Import bytes as big-endian
+            mpz_t result_as_num;
+            mpz_init(result_as_num);
+            mpz_import(result_as_num,       // rop
+                       DIGEST_SIZE_BYTES,   // count
+                       1,                   // order
+                       1,                   // size
+                       0,                   // endian
+                       0,                   // nails
+                       output_digest);      // op
+
+            // Convert to bigint, within F_p
+            libff::bigint<FieldT::num_limbs> item(result_as_num);
+            assert( sizeof(item.data) == DIGEST_SIZE_BYTES );
+            std::cout << "FieldT(\"";
+            item.print();//NB: for a good print remove the \n from libff print implementation
+            std::cout << "\"), "<< std::endl;
+
+            mpz_clear(result_as_num);
+
+        }
+        std::cout << "]";  
 }
 
 // namespace ethsnarks
@@ -92,6 +138,6 @@ int main( int argc, char **argv )
     ethsnarks::ppT::init_public_params();
     ethsnarks::generate_test_vector();
     ethsnarks::generate_mt(3);
-
+    ethsnarks::generate_sha3_constants();
     return 0;
 }
